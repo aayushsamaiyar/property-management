bcfffc220630f32e79295986e3185d4f728df062
aayushsamaiyar <aayushsamaiyar@gmail.com>
2022-11-20 02:18:56 +0530

    added logging with logback using slf4j

--- src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,7 +16,13 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "owner email is mandatory")
+    @NotEmpty(message = "password cannot be empty")
+    @Size(min = 1, max = 50, message = "owner email shhould be between 1 to 50 characters")
     private String ownerEmail;
     private String phone;
+    @NotNull
+    @Size(min=8, max=16, message = "password between 8 to 16")
+    @NotEmpty(message = "password cannot be empty")
     private String password;
 }
--- src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
@@ -1,18 +1,48 @@
 package com.mycompany.propertymanagement.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException manv){
+        List<ErrorModel> errorModelList = new ArrayList<>();
+        ErrorModel errorModel = null;
+        List<FieldError> fieldErrorList = manv.getFieldErrors();
+        for(FieldError fe: fieldErrorList){
+            logger.debug("inside Field validation: {} - {}",fe.getField(),fe.getDefaultMessage());
+            logger.info("inside Field validation: {} - {}",fe.getField(),fe.getDefaultMessage());
+            errorModel = new ErrorModel();
+            errorModel.setCode(fe.getField());
+            errorModel.setMessage(fe.getDefaultMessage());
+            errorModelList.add(errorModel);
+        }
+        return new ResponseEntity<List<ErrorModel>>(errorModelList, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException bex){
-        System.out.println("business exception is thrown");
+        for(ErrorModel er: bex.getErrors()){
+            logger.debug("BusinessException is thrown - level-debug: {} - {}",er.getCode(),er.getMessage());
+            logger.info("BusinessException is thrown - level-info: {} - {}",er.getCode(),er.getMessage());
+            logger.warn("BusinessException is thrown - level-warn: {} - {}",er.getCode(),er.getMessage());
+            logger.error("BusinessException is thrown - level-error: {} - {}",er.getCode(),er.getMessage());
+        }
+
         return new ResponseEntity<List<ErrorModel>>(bex.getErrors(), HttpStatus.BAD_REQUEST);
     }
 }
